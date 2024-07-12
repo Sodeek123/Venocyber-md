83a7c6b80ac48ccc89b21d7dcc110c358b8e8c10
Sodeek123 <babalawoifadele1@gmail.com>
2024-07-12 11:10:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ global.waPresence = process.env.WAPRESENCE || "online";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
@@ -42,7 +42,7 @@ global.scan = "https://rb.gy/3keql8";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  ""
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZUtiWjZ5bmVGak9uZXZOdU5kT0pJSG5TMWtYRlg2aUxoVzdiYm5SMDFXMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiekxoMCt2UzhUVVlhMXliQkoyYzFwNXhsWDBMbVVrLzhibnlZUDBtRmpqTT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI4QmxnazB6N3Y1UkpnYnh6NDFsWVNhVXMrU3ZBSHhyc3JaNHhRV04xaWtZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJtRUN1alM3bFFoNklaeHBhUXBhK2U2VjVFL3pBZ1h2azJNQ0JicUhYVHl3PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkdDMHJjZFdlcndIbGc2WUQ0ekZLUExyeG5CN0xSZHIzc1RwNWU3S0VRMzQ9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImwxMjVQRUxjY3JWMW82Z0xMQ0FGS2NpWVkrRDl5RmJiaUhrRlpMZGVreHc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiNk5xZDB2MUVISlh0Z1VxOXVMZ1IvbnczRW9KRzBWOUxMY0xUWm1jN0hIdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiVU1KblI3ejNxVXZGOG9wUkZaRUdIcU5hSnhJWUNnRUFuZUl3dkZQbWNIdz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjArWHQ2YSsrV3NvbnVGZU8wNkJGYWt4MzU4QisyTW50QTdzUEl1Ukd6RnBpbXJ5bFVURmVEc242b01hN3h0bDYwTHBDVDJZNjFleW1YckVVTlNzdWdRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTU5LCJhZHZTZWNyZXRLZXkiOiJRdjJmNnBCZG53MEF1V3RhbE96V08xYWVMdFFUQ3ViU2p6UlRlOGp5NHpvPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJwQlVkWlVKU1NDeVVLOTFVYlVMeDNRIiwicGhvbmVJZCI6IjUwNDFhMTkyLTk3M2YtNDk2Mi04Njk1LTUyN2U1Mzg1M2ZjZCIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIreEJManRSYmV4cGxmcklwTnZOMUlKWGdScFE9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRnpXdlR1ZStxeDQ5dlRkL21GZVJOZlh5MzRZPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlBDOTNGTEY4IiwibWUiOnsiaWQiOiIyMzQ3MDE3NzE2MzQ4OjJAcy53aGF0c2FwcC5uZXQiLCJuYW1lIjoiTGVnaXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ05qUHYwb1F3L3pEdEFZWUFTQUFLQUE9IiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Ikc3ZmNZWXMycTFUUkhQSVJJWjFHWE51SzJveXFTT1ZUV25mODZabVBuQms9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImJWZ3RkS0labm1NSzVtSlV0Y3gycUc4S1EybDZpNGhseERzQ2lpV2JYNC9CQ3I3TUpUT0VBclJnY0pQR0swYXMzOHduZ0tGTFdQVnhJQm1MWlo4WENBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiIrVDY5YmJ0eGlWd0tnYlY1azZYbG5DVFdQSDl5cU9CcEdrcFdMM1ZjOWRwRnJIUWUvdjh2NERFN2ROdDIyTnl6ekk4T0ZUMUFFMVJDK1NZUHFCUVBnUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDcwMTc3MTYzNDg6MkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJSdTMzR0dMTnF0VTBSenlFU0dkUmx6Yml0cU1xa2psVTFwMy9PbVpqNXdaIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwNzc4MzIyLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQURWRCJ9"
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -51,8 +51,8 @@ module.exports = {
   caption: process.env.CAPTION || "you're using first powerful bot to be released in Tanzania| enjoy your time| this is 𝐕𝐄𝐍𝐎𝐂𝐘𝐁𝐄𝐑 𝐌𝐃",
   author: process.env.PACK_AUTHER || "VENOCYBER",
   packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "VENOCYBER-𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "VENOCYBER",
+  botname: process.env.BOT_NAME || "Hardeaymorlar-𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "Legit",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
